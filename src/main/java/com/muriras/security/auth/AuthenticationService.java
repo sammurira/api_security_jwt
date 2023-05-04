ee0e219a44a6c0284563d674e0f592aea897c1aa
sammurira <sammurira@gmail.com>
2023-05-04 06:12:39 +0300

    Java JWT API Auth core - Initial Commit

---
+++ src/main/java/com/muriras/security/auth/AuthenticationService.java
@@ -0,0 +1,58 @@
+package com.muriras.security.auth;
+
+
+import com.muriras.security.config.JwtService;
+import com.muriras.security.user.Role;
+import com.muriras.security.user.User;
+import com.muriras.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                authenticationRequest.getEmail(),
+                authenticationRequest.getPassword()
+                )
+        );
+        var user = userRepository.findByEmail(authenticationRequest.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
---
+++ src/main/java/com/muriras/security/user/Role.java
@@ -0,0 +1,6 @@
+package com.muriras.security.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
